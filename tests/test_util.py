9f897e6370816ac645f6eef7c77417d33628fb9c
Miles Lucas <mdlucas@hawaii.edu>
2022-04-16 15:47:20 -1000

    add flc state filtering function

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,5 @@
 import pytest
-from vampires_dpp.util import frame_center
+from vampires_dpp.util import frame_center, flc_inds
 import numpy as np
 
 
@@ -16,3 +16,58 @@ def test_frame_center(frame, center):
     fcenter = frame_center(frame)
     assert fcenter[0] == center[0]
     assert fcenter[1] == center[1]
+
+
+def test_flc_inds_good():
+    states = np.array([1, 1, 2, 2, 3, 3, 4, 4, 1, 1, 2, 2, 3, 3, 4, 4])
+    inds = flc_inds(states, 2)
+    assert np.allclose(states[inds], states)
+
+    states = np.array([1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4])
+    inds = flc_inds(states, 4)
+    assert np.allclose(states[inds], states)
+
+
+def test_flc_inds_filter():
+    states = np.array([1, 1, 2, 2, 3, 3, 1, 1, 2, 2, 3, 3, 4, 4])
+    inds = flc_inds(states, 2)
+    assert np.allclose(inds, [6, 7, 8, 9, 10, 11, 12, 13])
+    assert np.allclose(states[inds], [1, 1, 2, 2, 3, 3, 4, 4])
+
+    states = np.array(
+        [
+            1,
+            1,
+            1,
+            1,
+            2,
+            2,
+            2,
+            2,
+            3,
+            3,
+            3,
+            3,
+            4,
+            4,
+            4,
+            4,
+            1,
+            1,
+            1,
+            1,
+            2,
+            2,
+            2,
+            2,
+            3,
+            3,
+            4,
+            4,
+            4,
+            4,
+        ]
+    )
+    inds = flc_inds(states, 4)
+    assert np.allclose(inds, range(16))
+    assert np.allclose(states[inds], [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4])
--- vampires_dpp/util.py
+++ vampires_dpp/util.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def frame_center(image):
     """
     Find the center of the frame or cube in pixel coordinates
@@ -15,3 +18,36 @@ def frame_center(image):
     ny = image.shape[-2]
     nx = image.shape[-1]
     return (ny - 1) / 2, (nx - 1) / 2
+
+
+def flc_inds(flc_states, n=4):
+    """
+    Find consistent runs of FLC states.
+
+    A consistent FLC run will have either 2 or 4 files per HWP state, and will have exactly 4 HWP states per cycle. Sometimes when VAMPIRES is syncing with CHARIS a HWP state will get skipped, creating partial HWP cycles. This function will return the indices which create consistent HWP cycles from the given list of FLC states, which should already be sorted by time.
+
+    Parameters
+    ----------
+    flc_states : list, ndarray
+        The FLC states to sort through
+    n : int, optional
+        The number of files per HWP state, either 2 or 4. By default 4
+
+    Returns
+    -------
+    inds :
+        The indices for which `flc_states` forms consistent HWP cycles
+    """
+    states = np.asarray(flc_states)
+    N_cycle = n * 4
+    state_list = np.repeat([1, 2, 3, 4], n)
+    inds = []
+    idx = 0
+    while idx <= len(flc_states) - N_cycle:
+        if np.all(states[idx : idx + N_cycle] == state_list):
+            inds.extend(range(idx, idx + N_cycle))
+            idx += N_cycle
+        else:
+            idx += 1
+
+    return inds
