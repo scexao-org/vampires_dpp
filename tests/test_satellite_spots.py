306b5ee717ade56afdfb7b411330007bc7af928b
Miles Lucas <mdlucas@hawaii.edu>
2022-04-17 17:40:32 -1000

    add function for getting slices of windows with proper bounds handling

--- tests/test_satellite_spots.py
+++ tests/test_satellite_spots.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from vampires_dpp.satellite_spots import window_centers
+from vampires_dpp.satellite_spots import window_centers, window_slice
 
 
 def test_window_centers():
@@ -23,3 +23,48 @@ def test_window_centers_two():
     centers = window_centers((50, 50), radius=10, theta=90, n=2)
     assert np.allclose(centers[0], [60, 50])
     assert np.allclose(centers[1], [40, 50])
+
+
+def test_window_slice():
+    frame = np.empty((101, 101))
+    sy, sx = window_slice(frame, center=(80, 50), window=30)
+    assert sy.start == 65
+    assert sy.stop == 95
+    assert sx.start == 35
+    assert sx.stop == 65
+
+
+def test_window_slice_window_shape():
+    frame = np.empty((101, 101))
+    sy, sx = window_slice(frame, center=(80, 50), window=(30, 40))
+    assert sy.start == 65
+    assert sy.stop == 95
+    assert sx.start == 30
+    assert sx.stop == 70
+
+
+def test_window_slice_out_of_bounds():
+    frame = np.empty((101, 101))
+    sy, sx = window_slice(frame, center=(90, 90), window=30)
+    assert sy.start == 75
+    assert sy.stop == 100
+    assert sx.start == 75
+    assert sx.stop == 100
+
+    sy, sx = window_slice(frame, center=(10, 90), window=30)
+    assert sy.start == 0
+    assert sy.stop == 25
+    assert sx.start == 75
+    assert sx.stop == 100
+
+    sy, sx = window_slice(frame, center=(10, 10), window=30)
+    assert sy.start == 0
+    assert sy.stop == 25
+    assert sx.start == 0
+    assert sx.stop == 25
+
+    sy, sx = window_slice(frame, center=(90, 10), window=30)
+    assert sy.start == 75
+    assert sy.stop == 100
+    assert sx.start == 0
+    assert sx.stop == 25
--- vampires_dpp/satellite_spots.py
+++ vampires_dpp/satellite_spots.py
@@ -12,11 +12,18 @@ def window_centers(center, radius, theta=0, n=4):
     return list(zip(ys, xs))
 
 
-def cutout_indices(frame, center, window):
-    pass
+def window_slice(frame, center, window):
+    half_width = np.asarray(window) / 2
+    Ny, Nx = frame.shape[-2:]
+    lower = np.maximum(0, np.round(center - half_width), dtype=int, casting="unsafe")
+    upper = np.minimum(
+        (Ny - 1, Nx - 1), np.round(center + half_width), dtype=int, casting="unsafe"
+    )
+    return slice(lower[0], upper[0]), slice(lower[1], upper[1])
 
 
 def window_indices(frame, window=30, center=None, **kwargs):
     if center is None:
         center = frame_center(frame)
     centers = window_centers(center, **kwargs)
+    slices = [window_slice(frame, center=cent, window=window) for cent in centers]
