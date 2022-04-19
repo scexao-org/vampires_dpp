1d06020d1e6953e4d6823b9204f961b1cf5c411b
Miles Lucas <mdlucas@hawaii.edu>
2022-04-18 16:28:57 -1000

    update calibration functions

--- vampires_dpp/calibration.py
+++ vampires_dpp/calibration.py
@@ -3,9 +3,17 @@
 from astropy.io import fits
 import numpy as np
 from pathlib import Path
+from typing import Optional
+from numpy.typing import ArrayLike
 
 
-def calibrate(data, discard=0, dark=None, flat=None, flip=False):
+def calibrate(
+    data: ArrayLike,
+    discard: int = 0,
+    dark: Optional[ArrayLike] = None,
+    flat: Optional[ArrayLike] = None,
+    flip: bool = False,
+):
     """
     Basic frame calibration.
 
@@ -13,20 +21,20 @@ def calibrate(data, discard=0, dark=None, flat=None, flip=False):
 
     Parameters
     ----------
-    data : ndarray
+    data : ArrayLike
         3-D cube (t, y, x) of data
     discard : int, optional
         The amount of leading frames to discard (for data which has destructive readout frames), by default 0
-    dark : ndarray, optional
+    dark : ArrayLike, optional
         If provided, will dark-subtract all frames by the provided 2-D master dark (y, x), by default None
-    flat : ndarray, optional
+    flat : ArrayLike, optional
         If provided, will flat-correct (after dark-subtraction) all frames by the provided 2-D master flat (y, x), by default None
     flip : bool, optional
         If True, will flip the x-axis of the data, for de-mirroring cam2 data, by default False
 
     Returns
     -------
-    ndarray
+    ArrayLike
         3-D calibrated data cube (t, y, x)
     """
     # discard frames
@@ -40,28 +48,39 @@ def calibrate(data, discard=0, dark=None, flat=None, flip=False):
     return out
 
 
-def calibrate_file(filename: str, suffix="_calib", hdu=0, **kwargs):
+def calibrate_file(
+    filename: str,
+    suffix="_calib",
+    hdu: int = 0,
+    dark: Optional[str] = None,
+    flat: Optional[str] = None,
+    **kwargs,
+):
     path = Path(filename)
     outname = path.with_name(f"{path.stem}{suffix}{path.suffix}")
-    with fits.open(path) as hdus:
-        data = hdus[hdu].data
-        hdr = hdus[hdu].header
-        processed = calibrate(data, **kwargs)
-        fits.writeto(outname, processed, hdr, overwrite=True)
+    data, hdr = fits.getdata(path, hdu, header=True)
+    if dark is not None:
+        hdr["VPP_DARK"] = (dark, "file used for dark subtraction")
+        dark_arr = fits.getdata(dark)
+    if flat is not None:
+        hdr["VPP_FLAT"] = (flat, "file used for flat-fielding")
+        flat_arr = fits.getdata(flat)
+    processed = calibrate(data, dark=dark_arr, flat=flat_arr, **kwargs)
+    fits.writeto(outname, processed, hdr, overwrite=True)
 
 
-def deinterleave(data):
+def deinterleave(data: ArrayLike):
     """
     Deinterleave data into two seperate FLC states
 
     Parameters
     ----------
-    data : ndarray
+    data : ArrayLike
         3-D data cube (t, y, x) from a single camera
 
     Returns
     -------
-    (state1, state2)
+    (state1, state2) : Tuple[ArrayLike, ArrayLike]
         two 3-D data cubes (t, y, x), one for every other frame from the original cube
     """
     set1 = data[::2]
@@ -69,19 +88,49 @@ def deinterleave(data):
     return set1, set2
 
 
-def deinterleave_file(filename: str, hdu=0, **kwargs):
+def deinterleave_file(filename: str, hdu: int = 0, **kwargs):
     path = Path(filename)
-    with fits.open(path) as hdus:
-        data = hdus[hdu].data
-        hdr = hdus[hdu].header
-        set1, set2 = deinterleave(data, **kwargs)
-        hdr1 = hdr.copy()
-        hdr1["U_FLCSTT"] = (1, "FLC state (1 or 2)")
-        hdr2 = hdr.copy()
-        hdr2["U_FLCSTT"] = (2, "FLC state (1 or 2)")
-        fits.writeto(
-            path.with_name(f"{path.stem}_FLC1{path.suffix}"), set1, hdr1, overwrite=True
-        )
-        fits.writeto(
-            path.with_name(f"{path.stem}_FLC2{path.suffix}"), set2, hdr2, overwrite=True
-        )
+    data, hdr = fits.getdata(path, hdu, header=True)
+    set1, set2 = deinterleave(data, **kwargs)
+    hdr1 = hdr.copy()
+    hdr1["U_FLCSTT"] = (1, "FLC state (1 or 2)")
+    hdr2 = hdr.copy()
+    hdr2["U_FLCSTT"] = (2, "FLC state (1 or 2)")
+    fits.writeto(
+        path.with_name(f"{path.stem}_FLC1{path.suffix}"), set1, hdr1, overwrite=True
+    )
+    fits.writeto(
+        path.with_name(f"{path.stem}_FLC2{path.suffix}"), set2, hdr2, overwrite=True
+    )
+
+
+def make_dark_file(filename: str, output: Optional[str] = None, discard: int = 1):
+    _path = Path(filename)
+    cube, header = fits.getdata(_path, header=True)
+    master_dark = np.median(cube[discard:], axis=0)
+    if output is not None:
+        outname = output
+    else:
+        outname = _path.with_name(f"{_path.stem}_master_dark{_path.suffix}")
+    fits.writeto(outname, master_dark, header=header, overwrite=True)
+
+
+def make_flat_file(
+    filename: str,
+    dark: Optional[str] = None,
+    output: Optional[str] = None,
+    discard: int = 1,
+):
+    _path = Path(filename)
+    cube, header = fits.getdata(_path, header=True)
+    cube = cube[discard:]
+    if dark is not None:
+        header["VPP_DARK"] = (dark, "file used for dark subtraction")
+        master_dark = fits.getdata(dark)
+        cube -= master_dark
+    master_flat = np.median(cube, axis=0)
+    if output is not None:
+        outname = output
+    else:
+        outname = _path.with_name(f"{_path.stem}_master_flat{_path.suffix}")
+    fits.writeto(outname, master_flat, header=header, overwrite=True)
