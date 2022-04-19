e4d99e52b513db279bc9c710d8068dd5943656e8
Miles Lucas <mdlucas@hawaii.edu>
2022-04-18 17:45:37 -1000

    add calibrate tests

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -3,6 +3,8 @@ from vampires_dpp.calibration import (
     deinterleave_file,
     make_dark_file,
     make_flat_file,
+    calibrate,
+    calibrate_file,
 )
 from astropy.io import fits
 import numpy as np
@@ -85,3 +87,35 @@ class TestCalibrationFrames:
         c, h = fits.getdata(tmp_path / "master_flat_cam1.fits", header=True)
         assert np.isclose(np.median(c), 1)
         assert h["VPP_DARK"] == dark_frame.name
+
+
+class TestCalibrate:
+    @pytest.fixture()
+    def dark_frame(self):
+        data = np.random.randn(512, 512) + 200
+        return data.astype("f4")
+
+    @pytest.fixture()
+    def flat_frame(self):
+        return np.ones((512, 512), dtype="f4")
+
+    @pytest.fixture()
+    def data_cube_cam1(self, dark_frame):
+        cube = 10 * np.random.randn(100, 512, 512) + dark_frame
+        cube += np.random.poisson(2e4, (100, 512, 512))
+        return cube.astype("uint16")
+
+    @pytest.fixture()
+    def data_cube_cam2(self, dark_frame):
+        cube = 10 * np.random.randn(100, 512, 512) + dark_frame
+        cube += np.random.poisson(2.3e4, (100, 512, 512))
+        cube = np.flip(cube, axis=1)
+        return cube.astype("uint16")
+
+    def test_calibrate(self, data_cube_cam1, data_cube_cam2, dark_frame, flat_frame):
+        calib1 = calibrate(data_cube_cam1, dark=dark_frame, flat=flat_frame)
+        calib2 = calibrate(data_cube_cam2, dark=dark_frame, flat=flat_frame, flip=True)
+        assert np.allclose(np.median(calib1, axis=(1, 2)), 2e4, rtol=1e-3)
+        assert np.allclose(np.median(calib2, axis=(1, 2)), 2.3e4, rtol=1e-3)
+        # if flip didn't work, they won't add together
+        assert np.allclose(np.median(calib1 + calib2, axis=(1, 2)), 4.3e4, rtol=1e-3)
