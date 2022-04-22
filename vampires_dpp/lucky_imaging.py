bcc46356624d073be5cb0468ee646be1ac44fab5
Miles Lucas <mdlucas@hawaii.edu>
2022-04-21 23:40:53 -1000

    update docstrings

--- vampires_dpp/lucky_imaging.py
+++ vampires_dpp/lucky_imaging.py
@@ -7,10 +7,12 @@ from skimage.transform import AffineTransform, warp
 from scipy.ndimage import fourier_shift
 
 
-from .image_processing import frame_center
+from .image_processing import frame_center, shift_frame
 
 
-def lucky_image(cube, q=0, metric="max", register="max", window=None, **kwargs):
+def lucky_image(
+    cube, q=0, metric="max", register="max", window=None, refidx=None, **kwargs
+):
     """
     Traditional lucky imaging
 
@@ -21,11 +23,13 @@ def lucky_image(cube, q=0, metric="max", register="max", window=None, **kwargs):
     q : float, optional
         The quantile for frame selection, by default 0. Must be between 0 and 1.
     register : str, optional
-        The algorithm for registering frames. "max" will align the peak pixel to the center, "com" will align the center of mass, and "dft" uses phase cross-correlation for registering frames. By default "max".
+        The algorithm for registering frames. "max" will align the peak pixel to the center, "com" will align the center of mass, and "dft" uses phase cross-correlation for registering frames. If None, will assume the cube is already coregistered. By default "max".
     metric : str, optional
-        The metric for which frame selection will be applied. The available metrics are "max" and "min". By default "max".
+        The metric for which frame selection will be applied. The available metrics are "max" and "l2norm". By default "l2norm".
     window : int, optional
         If not None, will only measure the frame selection metric in a centered box with this width, by default None.
+    refidx: ind, optional
+        If using DFT cross-correlation for registration, sets the reference index, or "best frame". If None, will find using the lucky-imaging metric, by default None.
     **kwargs
         additional keyword arguments will be passed to the registration method (e.g. `upsample_factor`)
     """
@@ -43,12 +47,17 @@ def lucky_image(cube, q=0, metric="max", register="max", window=None, **kwargs):
         tmp_cube = tmp_cube[values >= cut]
 
     center = frame_center(tmp_cube)
-    if register == "dft":
-        refidx = measure_metric(tmp_cube, metric).argmax()
+    # if using DFT upsampling, choose a reference index, or "best frame"
+    if register == "dft" and refidx is None:
+        if q > 0:
+            refidx = values.argmax()
+        else:
+            refidx = measure_metric(tmp_cube, metric).argmax()
         refframe = tmp_cube[refidx]
         refshift = np.unravel_index(refframe.argmax(), refframe.shape) - center
 
     out = np.zeros(tmp_cube.shape[1:], "f4")
+    # for each frame in time
     N = tmp_cube.shape[0]
     for i in range(tmp_cube.shape[0]):
         frame = tmp_cube[i]
@@ -63,9 +72,9 @@ def lucky_image(cube, q=0, metric="max", register="max", window=None, **kwargs):
             delta = refshift - phase_cross_correlation(
                 refframe, frame, return_error=False, **kwargs
             )
-
-        tform = AffineTransform(translation=delta[::-1])
-        shifted = warp(frame, tform)
+        # shift frame using Fourier phase offset
+        shifted = shift_frame(frame, delta)
+        # update mean online
         out += shifted / N
 
     return out
