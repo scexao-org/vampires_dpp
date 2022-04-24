fix rotation bug

tests/test_image_processing.py
@@ -26,12 +26,12 @@ def test_derotate_frame():
 
     cw_90 = derotate_frame(array, 90)
     expected = np.zeros_like(array)
-    expected[3, 2] = 1
+    expected[1, 2] = 1
     assert np.allclose(cw_90, expected)
 
     ccw_90 = derotate_frame(array, -90)
     expected = np.zeros_like(array)
-    expected[1, 2] = 1
+    expected[3, 2] = 1
     assert np.allclose(ccw_90, expected)
 
 
@@ -41,7 +41,7 @@ def test_derotate_frame_offset():
 
     cw_90 = derotate_frame(array, 90, center=(2, 2), mode="constant", cval=0)
     expected = np.zeros_like(array)
-    expected[3, 2] = 1
+    expected[1, 2] = 1
     assert np.allclose(cw_90, expected)
 
 
@@ -51,7 +51,7 @@ def test_derotate_frame_kwargs():
 
     cw_90 = derotate_frame(array, 90, order=1, mode="symmetric")
     expected = np.zeros_like(array)
-    expected[3, 2] = 1
+    expected[1, 2] = 1
     assert np.allclose(cw_90, expected)
 
 

vampires_dpp/image_processing.py
@@ -2,6 +2,7 @@ import numpy as np
 from skimage.transform import rotate
 from scipy.ndimage import fourier_shift
 from numpy.typing import ArrayLike
+from typing import Union
 
 
 def shift_frame(data: ArrayLike, shift):
@@ -23,8 +24,44 @@ def derotate_frame(data: ArrayLike, angle, center=None, **kwargs):
         "preserve_range": True,
         **kwargs,
     }
-    rotated = rotate(data, angle, **rotate_kwargs)
+    rotated = rotate(data, -angle, **rotate_kwargs)
     return rotated
+
+
+def derotate_cube(data: ArrayLike, angles: Union[ArrayLike, float], **kwargs):
+    # reverse user-given center because scikit-image
+    # uses swapped axes for this parameter only
+    angles = np.asarray(angles)
+    rotated = np.empty_like(data)
+    # if angles is a scalar, broadcoast along frame index
+    if angles.size == 1:
+        angles = np.full(rotated.shape[0], angles)
+    for idx in range(rotated.shape[0]):
+        rotated[idx] = derotate_frame(data[idx], angles[idx])
+    return rotated
+
+
+def weighted_collapse(
+    data: ArrayLike, angles: ArrayLike, fill_value: float = 0, **kwargs
+):
+    variance_frame = np.var(data, axis=0, keepdims=True)
+    # if the variance is zero, return the mean
+    if np.allclose(variance_frame, 0):
+        derotated = derotate_cube(data, angles, **kwargs)
+        return np.mean(derotated, 0)
+
+    # expand the variance frame into a cube
+    variance_cube = np.repeat(variance_frame, data.shape[0], axis=0)
+    # derotate both signal and variance
+    derotated_data = derotate_cube(data, angles, **kwargs)
+    derotated_variance = derotate_cube(variance_cube, angles, **kwargs)
+    # calculate weighted sum
+    numer = np.sum(derotated_data / derotated_variance, 0)
+    denom = np.sum(1 / derotated_variance, 0)
+    weighted_frame = numer / denom
+    # fix nans
+    weighted_frame[np.isnan(weighted_frame)] = fill_value
+    return weighted_frame
 
 
 def frame_center(image: ArrayLike):