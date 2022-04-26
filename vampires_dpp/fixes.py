39727a23a1ea17f7648e292ac9d0e2710cdf6e2a
Miles Lucas <mdlucas@hawaii.edu>
2022-04-26 12:55:52 -1000

    add docstrings

--- vampires_dpp/fixes.py
+++ vampires_dpp/fixes.py
@@ -4,6 +4,29 @@ from typing import Optional
 
 
 def fix_header(filename, output: Optional[str] = None):
+    """
+    Apply fixes to headers based on known bugs
+
+    Fixes
+    -----
+    1. Too many commas in timestamps
+        - Some STARS data have `UT` and `HST` timestamps like "10:03:34:342"
+        - In this case, the last colon is replaced with a comma, e.g. "10:03:34.342"
+    2. "typical" exposure values are file creation time instead of midpoint
+        - For `UT`, `HST`, and `MJD` keys will recalculate the midpoint based on the `*-STR` and `*-END` values
+
+    Parameters
+    ----------
+    filename : pathlike
+        Input FITS file
+    output : Optional[str], optional
+        Output file name, if None will append `_fix` to the input filename, by default None
+
+    Returns
+    -------
+    Path
+        path of the updated FITS file
+    """
     path = filename
     if output is None:
         output = path.with_name(f"{path.stem}_fix{path.suffix}")
@@ -26,6 +49,20 @@ def fix_header(filename, output: Optional[str] = None):
 
 
 def fix_typical_time_iso(hdr, key):
+    """
+    Return the middle point of the exposure for ISO-based timestamps
+
+    Parameters
+    ----------
+    hdr : FITSHeader
+    key : str
+        key to fix, e.g. "UT"
+
+    Returns
+    -------
+    str
+        The ISO timestamp (hh:mm:ss.sss) for the middle point of the exposure
+    """
     date = hdr["DATE-OBS"]
     # get start time
     key_str = f"{key}-STR"
@@ -41,6 +78,18 @@ def fix_typical_time_iso(hdr, key):
 
 
 def fix_typical_time_mjd(hdr):
+    """
+    Return the middle point of the exposure for MJD timestamps
+
+    Parameters
+    ----------
+    hdr : FITSHeader
+
+    Returns
+    -------
+    str
+        The MJD timestamp for the middle point of the exposure
+    """
     # repeat again for MJD with different format
     t_str = Time(hdr["MJD-STR"], format="mjd", scale="ut1")
     t_end = Time(hdr["MJD-END"], format="mjd", scale="ut1")
