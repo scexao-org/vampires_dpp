39519bb503de4e9a93db31ddeff166e69ff0b2f5
Miles Lucas <mdlucas@hawaii.edu>
2023-01-20 16:35:13 -1000

    update logo

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ html_theme = "alabaster"
 html_static_path = ["_static"]
 html_title = "VAMPIRES DPP"
 html_theme = "sphinx_book_theme"
-html_logo = "SCExAO_logo3.svg"
+html_logo = "scexao_logo.svg"
 html_theme_options = {
     "github_url": "https://github.com/scexao-org/vampires_dpp",
     "repository_url": "https://github.com/scexao-org/vampires_dpp",
