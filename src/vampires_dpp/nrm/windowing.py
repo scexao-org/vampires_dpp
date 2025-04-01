7946f40451a0b002c6fb8d07ea6b0ce44dfade87
Miles Lucas <mdlucas@hawaii.edu>
2025-03-31 14:56:21 -1000

    add windowing as pre-proc step to alignment

---
+++ src/vampires_dpp/nrm/windowing.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from vampires_dpp.indexing import frame_radii
+
+
+def super_gaussian(r, sigma, m, amp=1):
+    expon = np.log(2) * (2 ** (2 * m - 1)) * (r**2 / sigma**2) ** m
+    return amp * np.exp(-expon) ** 2
+
+
+def window_cube(cube, size, m=3, header=None):
+    radii = frame_radii(cube)
+    sigma = size * 2
+    window = super_gaussian(radii, sigma=sigma, m=m)
+
+    output = cube * window[None, ...]
+
+    if header is not None:
+        header["hieararch DPP NRM WINDOW SIZE"] = size, "[px] Gaussian window size"
+        header["hieararch DPP NRM WINDOW M"] = size, "Gaussian window scale"
+
+    return output, header
