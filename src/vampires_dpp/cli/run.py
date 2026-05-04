1d2176c42fb7a5d4fd4cc0e9b9fdf72f200c83a2
Miles Lucas <mdlucas@hawaii.edu>
2026-05-04 10:39:03 -0700

    only allow pdi to redo pdi stages

--- src/vampires_dpp/cli/run.py
+++ src/vampires_dpp/cli/run.py
@@ -101,7 +101,7 @@ def run(config: Path, filenames, num_proc, outdir, verbose, redo):
 @click.option(
     "--redo",
     default=None,
-    type=click.Choice(PIPELINE_STAGES),
+    type=click.Choice(["pdi"]),
     help="Force redo a pipeline stage; updated outputs cascade to downstream stages automatically.",
 )
 def pdi(config, filenames, num_proc, verbose, outdir, redo):
